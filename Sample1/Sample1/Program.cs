b39fbf19f3771a732cd9707e19f060bc04ce8482
viktoria.asboth <viktoria.asboth@semilab.hu>
2017-01-11 09:06:49 +0100

    Print product of two numbers.

--- Sample1/Sample1/Program.cs
+++ Sample1/Sample1/Program.cs
@@ -14,10 +14,14 @@ namespace Sample1
             int y = 4;
 
             int sum = x + y;
-            Console.WriteLine("Sum of {0} and {1} is", x, y, sum );
+            Console.WriteLine("Sum of {0} and {1} is {2}", x, y, sum );
 
             int diff = x - y;
-            Console.WriteLine("Difference of {0} and {1} is", x, y, diff);
+            Console.WriteLine("Difference of {0} and {1} is {2}", x, y, diff);
+
+            int prod = x * y;
+            Console.WriteLine("Product of {0} and {1} is {2}", x, y, prod);
+
             Console.ReadLine();
 
 
